bf277c8c93e80f695f533ecdbcf4d8ddd4e85610
hyperstone1 <kingofthegorgon@mail.ru>
2023-03-01 19:09:31 +0300

    changed navbar tabIndex

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,23 +10,18 @@ const Header = () => {
           <Navbar.Brand href="#home">
             <img className="logo" src={require('./logo-removebg-preview.png')} alt="logo" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand`}
-            aria-labelledby={`offcanvasNavbarLabel-expand`}
-            placement="end"
-            restoreFocus={false}
-          >
+          <Navbar.Toggle />
+          <Navbar.Offcanvas id="offcanvasNavbar" tabIndex="" placement="end" restoreFocus={false}>
             <Offcanvas.Header className="justify-content-end" closeButton></Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className="menu  flex-grow-1 pe-3">
+              <Nav className="menu flex-grow-1 pe-3">
                 <Nav.Link href="#process">Процесс</Nav.Link>
                 <Nav.Link href="#works">Наши работы</Nav.Link>
                 <Nav.Link href="#reviews">Отзывы</Nav.Link>
                 <Nav.Link href="#form_app">Оставить заявку</Nav.Link>
               </Nav>
               <Nav>
-                <Nav.Link href='tel:+375333333333'>+375 (33) 333-33-33</Nav.Link>
+                <Nav.Link href="tel:+375333333333">+375 (33) 333-33-33</Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
