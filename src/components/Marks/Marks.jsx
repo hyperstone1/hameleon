a5f3ee86af2001e56f00680deb825b1e97e44f0b
hyperstone1 <kingofthegorgon@mail.ru>
2023-03-02 02:32:29 +0300

    fixed marks

--- src/components/Marks/Marks.jsx
+++ src/components/Marks/Marks.jsx
@@ -4,28 +4,30 @@ import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
 
 const Marks = () => {
   return (
-    <div className="marks_container" data-auto-correct-mobile-width="false">
-      {[...Array(4)].map(() => (
-        <div className="t1003__item" data-auto-correct-mobile-width="false">
-          {[
-            ...Array(
-              'Гарантия 1 год',
-              'Качество',
-              'Быстрое изготовление',
-              'Пожаробезопасность',
-              'Работа без посредника',
-              'Доставка по всему миру',
-            ),
-          ].map((item) => (
-            <>
-              <div className="t-text t-text_md t1003__item-txt">{item}</div>
-              <IoMdCheckmarkCircleOutline
-                style={{ width: '25px', height: '25px', color: '#66ed44' }}
-              />
-            </>
-          ))}
-        </div>
-      ))}
+    <div className="marks">
+      <div className="marks_container" data-auto-correct-mobile-width="false">
+        {[...Array(4)].map(() => (
+          <div className="t1003__item" data-auto-correct-mobile-width="false">
+            {[
+              ...Array(
+                'Гарантия 1 год',
+                'Качество',
+                'Быстрое изготовление',
+                'Пожаробезопасность',
+                'Работа без посредника',
+                'Доставка по всему миру',
+              ),
+            ].map((item) => (
+              <>
+                <div className="t-text t-text_md t1003__item-txt">{item}</div>
+                <IoMdCheckmarkCircleOutline
+                  style={{ width: '25px', height: '25px', color: '#66ed44' }}
+                />
+              </>
+            ))}
+          </div>
+        ))}
+      </div>
 
       {/* <div className="t1003__item" data-auto-correct-mobile-width="false">
         <div className="t-text t-text_md t1003__item-txt">Гарантия 1 год</div>
